9093ef44984d7e027ab2179b45c3a49e0b95782f
Psy-Fer <fergo1988@gmail.com>
2023-08-17 18:44:45 +1000

    better descriptions and docs link

--- src/__init__.py
+++ src/__init__.py
@@ -4,12 +4,12 @@ from ._version import __version__
 
 modules = ['plot', 'reform', 'realign', 'plot_pileup', 'plot_tracks', 'calculate_offsets']
 module_help = {
-    'plot': {'help': 'stuff'},
-    'reform': {'help': 'stuff'},
-    'realign': {'help': 'stuff'},
-    'plot_pileup': {'help': 'stuff'},
-    'plot_tracks': {'help': 'stuff'},
-    'calculate_offsets': {'help': 'stuff'},
+    'plot': {'help': 'Plot read/reference - signal alignments.'},
+    'reform': {'help': "Convert basecaller's move table to ss string format."},
+    'realign': {'help': 'Realign signal to reference using cigar string and the move table.'},
+    'plot_pileup': {'help': 'Plot a reference - signal alignment pileup.'},
+    'plot_tracks': {'help': 'Plot multiple reference - signal alignment pileup tracks.'},
+    'calculate_offsets': {'help': 'A utility program to calculate the most significant base index given a kmer model or a read - signal alignment.'},
 }
 version = "squigualiser     {}".format(__version__)
 
@@ -17,7 +17,7 @@ def main():
     parser = ArgumentParser(
         description='squigualiser - A simple tool to Visualise nanopore raw signal-base alignment.',
         epilog='''
-See https://github.com/hiruna72/squigualiser/blob/main/docs/commands.md for detailed description of these command-line options.
+See slow5.page.link/squigualiser for a detailed description of these command-line options.
 
 Citation:...
                ''',
