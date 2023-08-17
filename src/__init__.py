f6286a4c3fd29e9d9bdce462be282db557a18fc3
Psy-Fer <fergo1988@gmail.com>
2023-08-17 16:30:09 +1000

    Better looking help, incomplete

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,27 @@
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, RawTextHelpFormatter
 from src import plot, reform, realign, plot_pileup, plot_tracks, calculate_offsets
 from ._version import __version__
 
 modules = ['plot', 'reform', 'realign', 'plot_pileup', 'plot_tracks', 'calculate_offsets']
+module_help = {
+    'plot': {'help': 'stuff'},
+    'reform': {'help': 'stuff'},
+    'realign': {'help': 'stuff'},
+    'plot_pileup': {'help': 'stuff'},
+    'plot_tracks': {'help': 'stuff'},
+    'calculate_offsets': {'help': 'stuff'},
+}
 version = "squigualiser     {}".format(__version__)
 
 def main():
     parser = ArgumentParser(
-        'squigualiser',
-        formatter_class=ArgumentDefaultsHelpFormatter
-    )
+        description='squigualiser - A simple tool to Visualise nanopore raw signal-base alignment.',
+        epilog='''
+See https://github.com/hiruna72/squigualiser/blob/main/docs/commands.md for detailed description of these command-line options.
+
+Citation:...
+               ''',
+        formatter_class=RawTextHelpFormatter)
 
     parser.add_argument(
         '-v', '--version', action='version',
@@ -23,7 +35,7 @@ def main():
     subparsers.required = True
     for module in modules:
         mod = globals()[module]
-        p = subparsers.add_parser(module, parents=[mod.argparser()])
+        p = subparsers.add_parser(module, help=module_help[module]['help'], parents=[mod.argparser()])
         p.set_defaults(func=mod.run)
 
     args = parser.parse_args()
