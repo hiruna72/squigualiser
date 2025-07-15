f0730231fbe2b859f1f0d06c7e0cb61848be119a
Hiruna Samarakoon <hiruna72@gmail.com>
2025-07-15 17:44:46 +1000

    bump python to 3.9 for packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,9 +8,13 @@ die (){
 apt-get update || die "apt-get update failed"
 apt install wget gcc make zlib1g-dev git -y || die "system tools install failed"
 
-wget https://github.com/indygreg/python-build-standalone/releases/download/20221220/cpython-3.8.16+20221220-x86_64-unknown-linux-gnu-install_only.tar.gz || die "python wget failed"
-tar xf cpython-3.8.16+20221220-x86_64-unknown-linux-gnu-install_only.tar.gz || die "untar python failed"
-python/bin/python3.8 -m venv squig-venv || die "create venv failed"
+# wget https://github.com/indygreg/python-build-standalone/releases/download/20221220/cpython-3.8.16+20221220-x86_64-unknown-linux-gnu-install_only.tar.gz || die "python wget failed"
+# tar xf cpython-3.8.16+20221220-x86_64-unknown-linux-gnu-install_only.tar.gz || die "untar python failed"
+
+wget https://github.com/indygreg/python-build-standalone/releases/download/20250712/cpython-3.9.23+20250712-x86_64-unknown-linux-gnu-install_only.tar.gz || die "python wget failed"
+tar xf cpython-3.9.23+20250712-x86_64-unknown-linux-gnu-install_only.tar.gz || die "untar python failed"
+
+python/bin/python3.9 -m venv squig-venv || die "create venv failed"
 source squig-venv/bin/activate || die "sourcing venc failed"
 pip install --upgrade pip || die "upgrade pip failed"
 export CC=gcc
@@ -26,8 +30,8 @@ fi
 
 find ./ -name __pycache__ -type d | xargs rm -r || die "removing pycache failed"
 mv squig-venv/bin/squigualiser python/bin/ || die "moving squigualiser to bin failed"
-cp -r squig-venv/lib/python3.8/site-packages/* python/lib/python3.8/site-packages/ || die "copying site-packages failed"
-sed -i "s/squig-venv\/bin\/python3.8/\/usr\/bin\/env python3.8/g" python/bin/squigualiser  || die "changing headerline failed"
+cp -r squig-venv/lib/python3.9/site-packages/* python/lib/python3.9/site-packages/ || die "copying site-packages failed"
+sed -i "s/squig-venv\/bin\/python3.9/\/usr\/bin\/env python3.9/g" python/bin/squigualiser  || die "changing headerline failed"
 git clone --depth 1 --branch main https://github.com/hiruna72/squigualiser.git  || die "Failed to clone squigualiser"
 
 cp -r squigualiser/docs python || die "docs copy failed"
--- scripts/package_macos.sh
+++ scripts/package_macos.sh
@@ -9,10 +9,13 @@ die (){
 # tar xf cpython-3.8.16+20230726-aarch64-apple-darwin-install_only.tar.gz || die "untar python failed"
 
 # Download Python standalone build for macOS x86_64
-wget https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.8.16+20230726-x86_64-apple-darwin-install_only.tar.gz || die "python wget failed"
-tar xf cpython-3.8.16+20230726-x86_64-apple-darwin-install_only.tar.gz || die "untar python failed"
+# wget https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.8.16+20230726-x86_64-apple-darwin-install_only.tar.gz || die "python wget failed"
+# tar xf cpython-3.8.16+20230726-x86_64-apple-darwin-install_only.tar.gz || die "untar python failed"
 
-python/bin/python3.8 -m venv squig-venv || die "create venv failed"
+wget https://github.com/indygreg/python-build-standalone/releases/download/20250712/cpython-3.9.23+20250712-x86_64-apple-darwin-install_only.tar.gz || die "python wget failed"
+tar xf cpython-3.9.23+20250712-x86_64-apple-darwin-install_only.tar.gz || die "untar python failed"
+
+python/bin/python3.9 -m venv squig-venv || die "create venv failed"
 source squig-venv/bin/activate || die "sourcing venc failed"
 pip3 install --upgrade pip || die "upgrade pip failed"
 export HTSLIB_CONFIGURE_OPTIONS="--enable-bz2=no --enable-lzma=no --with-libdeflate=no --enable-libcurl=no  --enable-gcs=no --enable-s3=no"
@@ -28,8 +31,8 @@ fi
 
 find ./ -name __pycache__ -type d | xargs rm -r || die "removing pycache failed"
 mv squig-venv/bin/squigualiser python/bin/ || die "moving squigualiser to bin failed"
-cp -r squig-venv/lib/python3.8/site-packages/* python/lib/python3.8/site-packages/ || die "copying site-packages failed"
-sed -i '' "1s/.*/#\!\/usr\/bin\/env python3.8/" python/bin/squigualiser
+cp -r squig-venv/lib/python3.9/site-packages/* python/lib/python3.9/site-packages/ || die "copying site-packages failed"
+sed -i '' "1s/.*/#\!\/usr\/bin\/env python3.9/" python/bin/squigualiser
 git clone --depth 1 --branch main https://github.com/hiruna72/squigualiser.git  || die "Failed to clone squigualiser"
 
 cp -r squigualiser/docs python || die "docs copy failed"
